f3bc8bdc6a552b40d1e73135f1711bcf447d941f
Taeim Kwon <taeim.kwon@blackwelllearning.co.uk>
2021-07-26 15:27:45 +0100

    update a test method

--- src/test/java/uk/co/blackwell/microservice/ebay/notification/EbayNotificationApplicationTests.java
+++ src/test/java/uk/co/blackwell/microservice/ebay/notification/EbayNotificationApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.ebaywebhookdemo;
+package uk.co.blackwell.microservice.ebay.notification;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EbayWebhookDemoApplicationTests {
+class EbayNotificationApplicationTests {
 
 	@Test
 	void contextLoads() {
